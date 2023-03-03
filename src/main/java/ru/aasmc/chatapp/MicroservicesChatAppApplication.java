f38a875a448a907e26406c7ee2a6c39a0a66bca0
Alex Alexeyev <aasmc@yandex.ru>
2023-03-03 14:47:50 +0300

    initial commit

---
+++ src/main/java/ru/aasmc/chatapp/MicroservicesChatAppApplication.java
@@ -0,0 +1,13 @@
+package ru.aasmc.chatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesChatAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesChatAppApplication.class, args);
+    }
+
+}
