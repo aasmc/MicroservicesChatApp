cbc4db5c7238617bda6aa70af0833c85f955aa47
Alex Alexeyev <aasmc@yandex.ru>
2023-03-05 01:22:16 +0300

    refactor to use annotation for shedlock

--- src/main/java/ru/aasmc/chatapp/service/SubscriptionService.java
+++ src/main/java/ru/aasmc/chatapp/service/SubscriptionService.java
@@ -2,15 +2,10 @@ package ru.aasmc.chatapp.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import net.javacrumbs.shedlock.core.LockConfiguration;
-import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import ru.aasmc.chatapp.dto.Subscription;
 import ru.aasmc.chatapp.dto.Subscription.Status;
-
-import java.time.Duration;
-import java.time.Instant;
 
 @Slf4j
 @Service
@@ -18,37 +13,24 @@ import java.time.Instant;
 public class SubscriptionService {
     private final UserService userService;
     private final StripeGateway stripeGateway;
-    private final LockProvider lockProvider;
 
+    // Ensures that only one instance of the application is performing
+    // the scheduled task. If the instance executing the task fails during
+    // the execution, there will be no fail over.
+    // need another table in DB. For Postgres:
+    // name, lock_until, locked_at, locked_by (see migrations for shedlock)
+    @SchedulerLock(name = "checkSubscriptions", lockAtLeastFor = "10s", lockAtMostFor = "20s")
     @Scheduled(cron = "*/30 * * * * *")
     public void checkSubscriptions() {
-        // Ensures that only one instance of the application is performing
-        // the scheduled task. If the instance executing the task fails during
-        // the execution, there will be no fail over.
-        // need another table in DB. For Postgres:
-        // name, lock_until, locked_at, locked_by (see migrations for shedlock)
-        LockConfiguration lockCfg = new LockConfiguration(
-                Instant.now(),
-                "checkSubscriptions",
-                Duration.ofSeconds(20),
-                Duration.ofSeconds(10)
-        );
-        lockProvider.lock(lockCfg).ifPresent(lock -> {
-            log.info("Checking subscriptions...");
-
-            try {
-                stripeGateway.getSubscriptions().forEach(sub -> {
-                    userService.getById(sub.getUserId()).ifPresent(user -> {
-                        if (sub.getStatus() == Status.ACTIVE && user.isExpired()) {
-                            userService.updateUser(user.setExpired(false));
-                        }
-                        if (sub.getStatus() == Status.PAST_DUE && !user.isExpired()) {
-                            userService.updateUser(user.setExpired(true));
-                        }                    });
-                });
-            } finally {
-                lock.unlock();
-            }
+        stripeGateway.getSubscriptions().forEach(sub -> {
+            userService.getById(sub.getUserId()).ifPresent(user -> {
+                if (sub.getStatus() == Status.ACTIVE && user.isExpired()) {
+                    userService.updateUser(user.setExpired(false));
+                }
+                if (sub.getStatus() == Status.PAST_DUE && !user.isExpired()) {
+                    userService.updateUser(user.setExpired(true));
+                }
+            });
         });
     }
 }
